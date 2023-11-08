cf97f3380d29d9352a8e11534eecde8082a551f5
Joj007 <71085151+Joj007@users.noreply.github.com>
2023-11-08 14:39:41 +0100

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,31 +18,32 @@ using System.IO;
 
 namespace orgWPF
 {
-    /// <summary>
+    ///<summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    ///</summary>
     public partial class MainWindow : Window
     {
         List<Organization> organizations = new List<Organization>();
-        List<Organization> filteredList;
 
 
         public MainWindow()
         {
-            
+
             InitializeComponent();
 
-            foreach (string line in File.ReadAllLines("organizations-100.csv").Skip(1)) {
+            foreach (string line in File.ReadAllLines("organizations-100.csv").Skip(1))
+            {
                 organizations.Add(new Organization(line.Split(';')));
             }
             dgAdatok.ItemsSource = organizations;
 
             cbOrszag.Items.Add("Minden ország");
-            organizations.Select(n => n.Country).Distinct().OrderBy(n=>n).ToList().ForEach(n=> cbOrszag.Items.Add(n));
-            cbOrszag.SelectedIndex = 0;
+            organizations.Select(n => n.Country).Distinct().OrderBy(n => n).ToList().ForEach(n => cbOrszag.Items.Add(n));
 
             cbEv.Items.Add("Minden évszám");
             organizations.Select(n => n.Founded).Distinct().OrderBy(n => n).ToList().ForEach(n => cbEv.Items.Add(n));
+
+            cbOrszag.SelectedIndex = 0;
             cbEv.SelectedIndex = 0;
 
         }
@@ -48,9 +51,9 @@ namespace orgWPF
 
         private void dgAdatok_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(dgAdatok.SelectedItem is Organization)
+            if (dgAdatok.SelectedItem is Organization)
             {
-                
+
                 Organization kivalasztott = dgAdatok.SelectedItem as Organization;
                 lblId.Content = kivalasztott.Id;
                 lblWeb.Content = kivalasztott.Website;
@@ -58,29 +61,43 @@ namespace orgWPF
             }
 
 
-            
+
         }
 
         private void SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            filteredList = organizations;
-            
+            List<Organization> filteredList = organizations;
             filteredList = evszures(filteredList);
             filteredList = orszagszures(filteredList);
-    
-            
-            lblLetszam.Content = filteredList.Sum(n=>n.EmployeesNumber);
+            dgAdatok.ItemsSource = filteredList;
+
+            lblLetszam.Content = filteredList.Sum(n => n.EmployeesNumber);
         }
 
         private List<Organization> orszagszures(List<Organization> szurendoLista)
         {
-            return (string)cbOrszag.SelectedItem != "Minden ország" ? szurendoLista.Where(n => n.Country == (string)cbOrszag.SelectedItem).ToList() : szurendoLista;
+            if (cbOrszag.SelectedItem == null || cbOrszag.SelectedItem.ToString() == "Minden ország")
+            {
+                return szurendoLista;
+            }
+            else
+            {
+                return szurendoLista.Where(n => n.Country == cbOrszag.SelectedItem.ToString()).ToList();
+            }
         }
         private List<Organization> evszures(List<Organization> szurendoLista)
         {
+            if (cbEv.SelectedItem==null || cbEv.SelectedItem.ToString() == "Minden évszám")
+            {
+                return szurendoLista;
+            }
+            else
+            {
+                return szurendoLista.Where(n => n.FoundedString == cbEv.SelectedItem.ToString()).ToList();
+            }
             
-            return (string)cbEv.Text != "Minden évszám" ? szurendoLista.Where(n => n.FoundedString == cbEv.Text).ToList() : szurendoLista;
         }
 
     }
 }
+
